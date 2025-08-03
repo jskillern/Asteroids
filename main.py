d9b434e81ad62a0628f318a358d03f91561268c1
jskillern <skillernjonathon95@gmail.com>
2025-08-03 19:58:59 -0400

    player rotation

--- main.py
+++ main.py
@@ -18,13 +18,15 @@ def main():
 
     game = 1
     while game == 1:
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
