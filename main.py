ad76a858d94233114558454babcf3a7e0b83049c
jskillern <skillernjonathon95@gmail.com>
2025-08-03 18:58:12 -0400

    game clock

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import pygame
 from constants import *
 
-pygame.init
+pygame.init()
+clock = pygame.time.Clock()
 
+dt = 0
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-game = 1
+    game = 1
+    while game == 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
-while game == 1:
-    screen.fill((0,0,0))
-    pygame.display.flip()
 
 if __name__ == "__main__":
     main()
