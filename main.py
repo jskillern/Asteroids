e9066587154e80a5e2f934028512d22ea0790000
jskillern <skillernjonathon95@gmail.com>
2025-08-04 10:50:44 -0400

    moving asteroids

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import pygame
 from constants import *
 from player import *
 from circleshape import *
+from asteroids import *
+from asteroidfield import *
 
 pygame.init()
 clock = pygame.time.Clock()
 
 dt = 0
-player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
 def main():
     print("Starting Asteroids!")
@@ -18,8 +19,15 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
+    asteroidfield = AsteroidField()
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     game = 1
     while game == 1:
@@ -28,8 +36,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         
 
